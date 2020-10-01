847f95c8dd4416637219725c9f670825de565812
DigruttolaGabriel <DigruttolaGabriel@gmail.com>
2020-10-01 19:51:58 -0300

    base

---
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RestauranteRepositoryImpl.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.tallerweb1.repositorios;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("restauranteRepository")
+public class RestauranteRepositoryImpl implements RestauranteRepository {
+
+}
