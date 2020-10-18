ac1b52f93a328c5cf3ca97f0ae0e9d984e52ed25
Gaston Perez <gastonperez-97@hotmail.com>
2020-10-18 17:15:02 -0300

    mejora de codigo en MenuDeRestauranteTest

--- src/test/java/ar/edu/unlam/tallerweb1/persistencia/MenuDeRestauranteTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/persistencia/MenuDeRestauranteTest.java
@@ -25,53 +25,6 @@ public class MenuDeRestauranteTest extends SpringTest {
 	@Inject
 	private RestauranteRepository repositorioRestaurante;
 	
-	@Inject
-	private RestauranteService servicioRestaurante;
-	
-	public void crearComidas() {
-        ComidaModel comida1 = new ComidaModel();
-        ComidaModel comida2 = new ComidaModel();
-        ComidaModel comida3 = new ComidaModel();
-        ComidaModel comida4 = new ComidaModel();
-        
-        comida1.setNombre("comida1");
-        comida2.setNombre("comida2");
-        comida3.setNombre("comida3");
-        comida4.setNombre("comida4");
-        
-        comida1.setIdComida(1L);
-        comida2.setIdComida(2L);
-        comida3.setIdComida(3L);
-        comida4.setIdComida(4L);
-        
-        session().save(comida1);
-        session().save(comida2);
-        session().save(comida3);
-        session().save(comida4);
-	}
-	
-	public void crearRestaurantes() {
-        RestauranteModel restaurante1 = new RestauranteModel();
-        RestauranteModel restaurante2 = new RestauranteModel();
-        RestauranteModel restaurante3 = new RestauranteModel();
-        RestauranteModel restaurante4 = new RestauranteModel();
-        
-        restaurante1.setNombre("restaurante1");
-        restaurante2.setNombre("restaurante2");
-        restaurante3.setNombre("restaurante3");
-        restaurante4.setNombre("restaurante4");
-        
-        restaurante1.setIdRestaurante(1L);
-        restaurante2.setIdRestaurante(2L);
-        restaurante3.setIdRestaurante(3L);
-        restaurante4.setIdRestaurante(4L);
-        
-        session().save(restaurante1);
-        session().save(restaurante2);
-        session().save(restaurante3);
-        session().save(restaurante4);
-	}
-	
     @Test
     @Transactional @Rollback
     public void pruebaConexion(){
